05893510bd0a9a88d8ea3371fc269325b01dbb26
akbarzikritmpn <akbarzikri942@gmail.com>
2025-10-19 09:35:39 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -195,7 +195,7 @@ def halaman_main():
             if mode == "Deteksi Objek (YOLO)":
                 results = yolo_model(img_array)
                 img_with_boxes = img_array.copy()
-                class_names = class_names[label_index]
+                class_names = yolo_model.names
 
                 detected_objects = []
 
